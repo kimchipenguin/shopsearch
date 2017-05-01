8b8ecd633a0f814829252b562d0033007e923894
Matthias Jaap <miajaap@Matthiass-MBP.fritz.box>
2017-05-02 00:04:52 +0200

    first commit

---
+++ haendlersuche/ShopModel.swift
@@ -0,0 +1,35 @@
+//
+//  ShopModel.swift
+//  haendlersuche
+//
+//  Created by Mia Jaap on 21.11.16.
+//  Copyright © 2016 Matthias Jaap. All rights reserved.
+//
+
+import UIKit
+
+class ShopModel: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ haendlersuche/ShopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopTableViewCell.swift
+//  haendlersuche
+//
+//  Created by Mia Jaap on 21.11.16.
+//  Copyright © 2016 Matthias Jaap. All rights reserved.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
